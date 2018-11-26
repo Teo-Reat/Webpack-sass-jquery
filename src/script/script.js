c6ad78e44f9c1ba4c7c60b67e4ca7429af419d25
Teo-Reat <teo.reat@gmail.com>
2018-11-26 12:04:55 +0400

    Deleted 'dist'. Little fixes

--- src/script/script.js
+++ src/script/script.js
@@ -2,4 +2,7 @@
 // тут тихо и уютно
 // мы всегда будем уверены, что $ === jQuery
 // a undefined не переопределен ;)
+    $('.button').click(function() {
+        alert('Hello!');
+    });
 })(jQuery);
